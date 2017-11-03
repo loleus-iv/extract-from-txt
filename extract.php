bd24b23cc734b7b956c19739bc9618a1ab3584e4
Karol Aniszewski <karol.aniszewski@gmail.com>
2017-11-03 16:15:54 +0100

    Create extract.php

---
+++ extract.php
@@ -0,0 +1,29 @@
+<?php
+  //load txt file
+  //in future in loop, all files from current directory
+
+  //one file is one line/record of data separated with semicolons
+  
+  //declare data to extract / names of attributes
+    //Wysokość: Szerokość: Grubość: Kamień: Symbol: Stan: 
+    //Akrostych: Przeliczenie daty: Metryka: 
+    //Metryka zgonu: data separated with commas
+      /*
+      Nr 307, Tarnów dnia 25 października 1877, no 751,
+      lekarz: Dr Metzger,
+      godzina ½ 1 w nocy,
+      data zgonu: 25 października 1877,
+      miejsce zgonu: Tarnów, zamieście no 100/101,
+      data pogrzebu: 27 października 1877,
+      miejsce pogrzebu: Tarnów, zamieście Pogwizdów,
+      zmarła: NECHE WULWEK, żona Chajma Wulwek faktora,
+      rodzice: Chaym Sternreich Tarnów,
+      miejsce zamieszkania: Tarnów 100/101, płeć żeńska, lat 29, zamężna
+      przyczyna zgonu: zapalenie trzewy
+      */
+      
+      
+      
+      
+  //print data OR/AND save to the file as next line/record
+?>
